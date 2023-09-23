0eff543c2975bdf0ddac95d66a8651e188d9f454
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-09-23 23:22:40 +0200

    Open server websocket

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws'
+import { Message } from './interfaces/Message'
 
-const port = 1234;
-const wss = new WebSocketServer({ port });
+const port = 1234
+const wss = new WebSocketServer({ port })
+const message: Message = {
+  user: 'Jane doe',
+  message: 'I was sent using server resonse',
+  date: 'Sat Sep 23 2023 12:25:32 GMT+0200 (Central European Summer Time)',
+}
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    console.log('Recieved: ', data)
+    ws.send(JSON.stringify(message))
+  })
+})
