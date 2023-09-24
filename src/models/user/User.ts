bc5024b3a4a24e7e872976097619159f0dc322d0
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-09-24 19:15:34 +0200

    Create User schema

---
+++ src/models/user/User.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  role: { type: String, required: true, default: 'user' },
+  password: { type: String, required: true },
+})
+
+export const User = mongoose.model('User', userSchema)
