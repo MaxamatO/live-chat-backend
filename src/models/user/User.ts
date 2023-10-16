97f3fe377035203dd4f1c5b2c682eb859ed6d007
maks <pierchala.maks@gmail.com>
2023-10-16 08:34:39 +0200

    Add email field

--- src/models/user/User.ts
+++ src/models/user/User.ts
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   username: { type: String, required: true },
+  email: { type: String, required: true },
   role: { type: String, required: true, default: 'user' },
   password: { type: String, required: true },
 })
