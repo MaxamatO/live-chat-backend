b5c5333a9550010f7173db5170f50f43b0c0f3ab
maks <pierchala.maks@gmail.com>
2023-10-16 08:34:00 +0200

    Add user seed

--- src/seed.ts
+++ src/seed.ts
@@ -1,6 +1,11 @@
 import { User } from './models/user/User'
 
-export const seed = () => {
-  User.deleteMany()
-  User.create({ username: 'Max', password: 'test', role: 'Admin' })
+export const seed = async () => {
+  await User.deleteMany({})
+  User.create({
+    username: 'Max',
+    email: 'test@test.com',
+    password: 'test',
+    role: 'Admin',
+  })
 }
